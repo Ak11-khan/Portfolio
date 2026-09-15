cce744e1ab7ba4ea6c1c6dfd1e3c1ce058303ff8
Ak11-khan <m.arfakhan@protonmail.com>
2026-09-15 19:55:20 -0400

    Rename image utility and update import

--- src/Router/Projects/Projects.jsx
+++ src/Router/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import projects from "../../data/projects.json";
-import { getImageUrl } from "../../utils";
+import { getImageUrl } from "../../imageutils";
 import style from "./Projects.module.css";
 
 const Projects = () => {
