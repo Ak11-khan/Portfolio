3a44cd7f4dda91308de3862d2ded05a61b169b8e
Ak11-khan <m.arfakhan@protonmail.com>
2026-09-15 19:20:09 -0400

    Fix utils import case

--- src/Router/Projects/Projects.jsx
+++ src/Router/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import projects from "../../data/projects.json";
-import { getImageUrl } from "../../Utils";
+import { getImageUrl } from "../../utils";
 import style from "./Projects.module.css";
 
 const Projects = () => {
